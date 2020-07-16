f483067e9d6c39925dd85601c60bc52f6cbe8f26
EC2 Default User <ec2-user@ip-172-31-91-31.ec2.internal>
2020-07-16 01:07:24 +0000

    working thru keras

--- data_science_py/25_nn/regerssion_keras.py
+++ data_science_py/25_nn/regerssion_keras.py
@@ -7,7 +7,8 @@
 
 # # Keras Regression Code Along Project 
 # 
-# Let's now apply our knowledge to a more realistic data set. Here we will also focus on feature engineering and cleaning our data!
+# Let's now apply our knowledge to a more realistic data set. Here we will also focus 
+# on feature engineering and cleaning our data!
 
 # ## The Data
 # 
@@ -28,7 +29,9 @@
 # * waterfront - A dummy variable for whether the apartment was overlooking the waterfront or not
 # * view - An index from 0 to 4 of how good the view of the property was
 # * condition - An index from 1 to 5 on the condition of the apartment,
-# * grade - An index from 1 to 13, where 1-3 falls short of building construction and design, 7 has an average level of construction and design, and 11-13 have a high quality level of construction and design.
+# * grade - An index from 1 to 13, where 1-3 falls short of building construction and design, 
+# 7 has an average level of construction and design, and 11-13 have a high quality level 
+# of construction and design.
 # * sqft_above - The square footage of the interior housing space that is above ground level
 # * sqft_basement - The square footage of the interior housing space that is below ground level
 # * yr_built - The year the house was initially built
@@ -38,307 +41,124 @@
 # * long - Longitude
 # * sqft_living15 - The square footage of interior housing living space for the nearest 15 neighbors
 # * sqft_lot15 - The square footage of the land lots of the nearest 15 neighbors
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
-# In[2]:
-
-
+PATH = '/home/ec2-user/environment/udemy_courses/data_science_py/25_nn/figs/'
 df = pd.read_csv('../data/kc_house_data.csv')
 
-
 # # Exploratory Data Analysis
-
-# In[3]:
-
-
-df.isnull().sum()
-
-
-# In[4]:
-
-
-df.describe().transpose()
-
-
-# In[5]:
-
-
+print(df.isnull().sum())
+print(df.describe().transpose())
 plt.figure(figsize=(12,8))
 sns.distplot(df['price'])
-
-
-# In[6]:
-
+plt.savefig(PATH + 'price.png', dpi=300)
+plt.close()
 
 sns.countplot(df['bedrooms'])
-
-
-# In[7]:
-
+plt.savefig(PATH + 'bedrooms.png', dpi=300)
+plt.close()
 
 plt.figure(figsize=(12,8))
 sns.scatterplot(x='price',y='sqft_living',data=df)
-
-
-# In[8]:
-
+plt.savefig(PATH + 'sqft.png', dpi=300)
+plt.close()
 
 sns.boxplot(x='bedrooms',y='price',data=df)
-
+plt.savefig(PATH + 'bed_px.png', dpi=300)
+plt.close()
 
 # ### Geographical Properties
-
-# In[9]:
-
-
 plt.figure(figsize=(12,8))
 sns.scatterplot(x='price',y='long',data=df)
-
-
-# In[10]:
-
+plt.savefig(PATH + 'price_long.png', dpi=300)
+plt.close()
 
 plt.figure(figsize=(12,8))
 sns.scatterplot(x='price',y='lat',data=df)
-
-
-# In[11]:
-
+plt.savefig(PATH + 'price_lat.png', dpi=300)
+plt.close()
 
 plt.figure(figsize=(12,8))
 sns.scatterplot(x='long',y='lat',data=df,hue='price')
+plt.savefig(PATH + 'long_lat.png', dpi=300)
+plt.close()
 
-
-# In[12]:
-
-
-df.sort_values('price',ascending=False).head(20)
-
-
-# In[13]:
-
-
-len(df)*(0.01)
-
-
-# In[14]:
-
-
+print(df.sort_values('price',ascending=False).head(20))
+print(len(df)*(0.01))
 non_top_1_perc = df.sort_values('price',ascending=False).iloc[216:]
-
-
-# In[15]:
-
 
 plt.figure(figsize=(12,8))
 sns.scatterplot(x='long',y='lat',
                 data=non_top_1_perc,hue='price',
                 palette='RdYlGn',edgecolor=None,alpha=0.2)
-
+plt.savefig(PATH + 'non_1_perc.png', dpi=300)
+plt.close()
 
 # ### Other Features
-
-# In[16]:
-
-
 sns.boxplot(x='waterfront',y='price',data=df)
-
+plt.savefig(PATH + 'waterfront.png', dpi=300)
+plt.close()
 
 # ## Working with Feature Data
-
-# In[17]:
-
-
-df.head()
-
-
-# In[18]:
-
-
-df.info()
-
-
-# In[19]:
-
-
+print(df.head())
+print(df.info())
 df = df.drop('id',axis=1)
-
-
-# In[20]:
-
-
-df.head()
-
+print(df.head())
 
 # ### Feature Engineering from Date
-
-# In[21]:
-
-
 df['date'] = pd.to_datetime(df['date'])
-
-
-# In[22]:
-
-
 df['month'] = df['date'].apply(lambda date:date.month)
-
-
-# In[23]:
-
-
 df['year'] = df['date'].apply(lambda date:date.year)
-
-
-# In[24]:
-
-
 sns.boxplot(x='year',y='price',data=df)
-
-
-# In[25]:
-
+plt.savefig(PATH + 'year.png', dpi=300)
+plt.close()
 
 sns.boxplot(x='month',y='price',data=df)
-
-
-# In[26]:
-
+plt.savefig(PATH + 'month_px.png', dpi=300)
+plt.close()
 
 df.groupby('month').mean()['price'].plot()
-
-
-# In[27]:
-
+plt.savefig(PATH + 'month_group.png', dpi=300)
+plt.close()
 
 df.groupby('year').mean()['price'].plot()
-
-
-# In[28]:
-
+plt.savefig(PATH + 'year_px.png', dpi=300)
+plt.close()
 
 df = df.drop('date',axis=1)
-
-
-# In[29]:
-
-
-df.columns
-
-
-# In[30]:
-
+print(df.columns)
 
 # https://i.pinimg.com/originals/4a/ab/31/4aab31ce95d5b8474fd2cc063f334178.jpg
 # May be worth considering to remove this or feature engineer categories from it
-df['zipcode'].value_counts()
-
-
-# In[31]:
-
-
+print(df['zipcode'].value_counts())
 df = df.drop('zipcode',axis=1)
-
-
-# In[32]:
-
-
-df.head()
-
-
-# In[33]:
-
+print(df.head())
 
 # could make sense due to scaling, higher should correlate to more value
-df['yr_renovated'].value_counts()
-
-
-# In[34]:
-
-
-df['sqft_basement'].value_counts()
-
+print(df['yr_renovated'].value_counts())
+print(df['sqft_basement'].value_counts())
 
 # ## Scaling and Train Test Split
-
-# In[35]:
-
-
 X = df.drop('price',axis=1)
 y = df['price']
 
-
-# In[36]:
-
-
 from sklearn.model_selection import train_test_split
-
-
-# In[37]:
-
-
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,random_state=101)
 
-
 # ### Scaling
-
-# In[38]:
-
-
 from sklearn.preprocessing import MinMaxScaler
-
-
-# In[39]:
-
-
 scaler = MinMaxScaler()
-
-
-# In[40]:
-
-
 X_train= scaler.fit_transform(X_train)
-
-
-# In[41]:
-
-
 X_test = scaler.transform(X_test)
-
-
-# In[42]:
-
-
-X_train.shape
-
-
-# In[43]:
-
-
-X_test.shape
-
+print(X_train.shape)
+print(X_test.shape)
 
 # ## Creating a Model
-
-# In[44]:
-
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from tensorflow.keras.optimizers import Adam
-
-
-# In[45]:
-
-
 model = Sequential()
 
 model.add(Dense(19,activation='relu'))
@@ -349,134 +169,51 @@ model.add(Dense(1))
 
 model.compile(optimizer='adam',loss='mse')
 
-
 # ## Training the Model
-
-# In[46]:
-
-
 model.fit(x=X_train,y=y_train.values,
           validation_data=(X_test,y_test.values),
           batch_size=128,epochs=400)
 
-
-# In[47]:
-
-
 losses = pd.DataFrame(model.history.history)
-
-
-# In[48]:
-
-
 losses.plot()
-
+plt.savefig(PATH + 'losses.png', dpi=300)
+plt.close()
 
 # # Evaluation on Test Data
 # 
 # https://scikit-learn.org/stable/modules/model_evaluation.html#regression-metrics
-
-# In[49]:
-
-
 from sklearn.metrics import mean_squared_error,mean_absolute_error,explained_variance_score
 
-
 # #### Predicting on Brand New Data
-
-# In[50]:
-
-
-X_test
-
-
-# In[51]:
-
-
+print(X_test)
 predictions = model.predict(X_test)
+print(mean_absolute_error(y_test,predictions))
+print(np.sqrt(mean_squared_error(y_test,predictions)))
+print(explained_variance_score(y_test,predictions))
 
-
-# In[52]:
-
-
-mean_absolute_error(y_test,predictions)
-
-
-# In[53]:
-
-
-np.sqrt(mean_squared_error(y_test,predictions))
-
-
-# In[54]:
-
-
-explained_variance_score(y_test,predictions)
-
-
-# In[55]:
-
-
-df['price'].mean()
-
-
-# In[56]:
-
-
-df['price'].median()
-
-
-# In[57]:
-
+print(df['price'].mean())
+print(df['price'].median())
 
 # Our predictions
 plt.scatter(y_test,predictions)
+plt.savefig(PATH + 'y_preds.png', dpi=300)
+plt.close()
 
 # Perfect predictions
 plt.plot(y_test,y_test,'r')
-
-
-# In[58]:
-
+plt.savefig(PATH + 'perf_preds.png', dpi=300)
+plt.close()
 
 errors = y_test.values.reshape(6480, 1) - predictions
-
-
-# In[59]:
-
-
 sns.distplot(errors)
-
+plt.savefig(PATH + 'errors.png', dpi=300)
+plt.close()
 
 # -------------
 # ### Predicting on a brand new house
-
-# In[81]:
-
-
 single_house = df.drop('price',axis=1).iloc[0]
-
-
-# In[86]:
-
-
 single_house = scaler.transform(single_house.values.reshape(-1, 19))
-
-
-# In[87]:
-
-
-single_house
-
-
-# In[89]:
-
+print(single_house)
 
 model.predict(single_house)
-
-
-# In[90]:
-
-
-df.iloc[0]
-
+print(df.iloc[0])
